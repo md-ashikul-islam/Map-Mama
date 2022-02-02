1935b8e0798ad9be6eda1264c9bfc186785c78ba
Md Ashikul Islam <md.ashikul.islam@g.bracu.ac.bd>
2022-02-03 05:19:17 +0600

    Implemented proper marker and location form

--- script.js
+++ script.js
@@ -11,14 +11,64 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
+
 if (navigator.geolocation)
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      const { latitude } = position.coords;
-      const { longitude } = position.coords;
-      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
-    },
-    function () {
-      alert('Could not get your position');
-    }
-  );
+  navigator.geolocation.getCurrentPosition(function (position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+    const coords = [latitude, longitude];
+    map = L.map('map').setView(coords, 20);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    //Handling Clicks on map
+    map.on(
+      'click',
+      function (mapE) {
+        mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
+      },
+      function () {
+        alert('Could not get your position');
+      }
+    );
+
+    form.addEventListener('submit', function (event) {
+      event.preventDefault();
+
+      //Clear Input Fields
+      inputDistance.value =
+        inputDuration.value =
+        inputCadence.value =
+        inputElevation.value =
+          '';
+
+      //Display Marker
+      const { lat, lng } = mapEvent.latlng;
+      L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(
+          L.popup({
+            maxWidth: 200,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup',
+          })
+        )
+        .setPopupContent('Workout')
+        .openPopup();
+    });
+  });
+
+inputType.addEventListener('change', function () {
+  inputElevation.closest('.form__row ').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row ').classList.toggle('form__row--hidden');
+});
